cd59a00a8951351d51ddd7b28c9dae08d86ae8f9
Bartosz Kostka <kostka.bartosz@gmail.com>
2020-02-20 19:30:14 +0100

    pro updated

--- kamil/pro.cpp
+++ kamil/pro.cpp
@@ -53,23 +53,81 @@ int main() {
   for(int il = 0; il < L; ++il) {
     libs[il].read();
   }
-  vector<int> his;
-  long long can = (long long) libs[0].per_day * (D - libs[0].signup_time);
-  for(int book : libs[0].books) {
-    if(can > 0) {
-      can--;
-      his.push_back(book);
+  vector<bool> scanned(B), ready(L);
+  int day = 0;
+  vector<pair<int, vector<int>>> strategy;
+  long long total_score = 0;
+  while(true) {
+    // debug() << imie(day);
+    // cerr << day << " ";
+    pair<long long, pair<int, vector<int>>> best;
+    best.first = 0;
+    for(int il = 0; il < L; ++il) {
+      if(!ready[il]) {
+        vector<int> remaining;
+        for(int b : libs[il].books) {
+          if(!scanned[b]) {
+            remaining.push_back(b);
+          }
+        }
+        sort(remaining.begin(), remaining.end(), [&](int a, int b) {
+          return score[a] > score[b];
+        });
+        long long can = max(0, D - day - libs[il].signup_time) * (long long) libs[il].per_day;
+        if(can < (long long) remaining.size()) {
+          remaining.resize(can);
+        }
+        long long gain = 0;
+        for(int b : remaining) {
+          gain += score[b];
+        }
+        gain = gain * 1000 * 1000 / pow(libs[il].signup_time, 1.5);
+        // assert(gain <= 1000 * 1000 * 1000);
+        if(gain > best.first) {
+          best = {(int) gain, {il, remaining}};
+        }
+      }
     }
+    if(best.first == 0) {
+      break;
+    }
+    // total_score += best.first;
+    int il = best.second.first;
+    ready[il] = true;
+    strategy.emplace_back(il, best.second.second);
+    for(int b : best.second.second) {
+      assert(!scanned[b]);
+      scanned[b] = true;
+      total_score += score[b];
+    }
+    day += libs[il].signup_time;
   }
-  puts("1"); // ile bibliotek
-  printf("%d %d\n", 0, (int) his.size());
-  long long result = 0;
-  for(int x : his) {
-    printf("%d ", x);
-    result += score[x];
+  cerr << endl;
+  printf("%d\n", (int) strategy.size());
+  for(pair<int, vector<int>> pp : strategy) {
+    printf("%d %d\n", pp.first, (int) pp.second.size());
+    for(int x : pp.second) {
+      printf("%d ", x);
+    }
+    puts("");
   }
-  puts("");
-  cerr << "score = " << result << endl;
+  // vector<int> his;
+  // long long can = (long long) libs[0].per_day * (D - libs[0].signup_time);
+  // for(int book : libs[0].books) {
+    // if(can > 0) {
+      // can--;
+      // his.push_back(book);
+    // }
+  // }
+  // puts("1"); // ile bibliotek
+  // printf("%d %d\n", 0, (int) his.size());
+  // long long result = 0;
+  // for(int x : his) {
+    // printf("%d ", x);
+    // result += score[x];
+  // }
+  // puts("");
+  cerr << "score = " << total_score / 1000. / 1000. << endl;
 }
 
 
